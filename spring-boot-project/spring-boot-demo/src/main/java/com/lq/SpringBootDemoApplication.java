58928c9d76d46cb0b7aeb4ed0920336b5cd1b999
liqiang <wbliqiang@centaline.com.cn>
2025-11-19 15:28:23 +0800

    spring-boot-2.2.5.RELEASE版本 带注释源码提交

---
+++ spring-boot-project/spring-boot-demo/src/main/java/com/lq/SpringBootDemoApplication.java
@@ -0,0 +1,16 @@
+package com.lq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liqiang
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+}
